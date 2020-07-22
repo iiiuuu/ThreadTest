e9a390c64cdd70cba7a6c4d0bc14a11d052ba43a
Long <longxu1@lenovo.com>
2020-07-22 19:21:40 +0800

    add multiple view

--- app/src/main/java/com/example/recyclerview/Data.java
+++ app/src/main/java/com/example/recyclerview/Data.java
@@ -5,19 +5,30 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Data {
+    public static final int TYPE_ITEM = 0;
+    public static final int TYPE_HEADER = 1;
+    public int type;
     public String title;
     public String description;
     public int number;
 
-    public Data(String title, String description, int number) {
+
+    public Data(int type, String title, String description, int number) {
+        this.type = type;
         this.title = title;
         this.description = description;
         this.number = number;
     }
 
     public static List<Data> getDataList(int size) {
-        return IntStream.rangeClosed(1, size)
-                .mapToObj(i -> new Data("title" + i, "desp" + i, i))
+        return IntStream.rangeClosed(0, size)
+                .mapToObj(i -> {
+                    if (i == 0) {
+                        return new Data(TYPE_HEADER, "This is Header", "desp" + i, i);
+                    } else {
+                        return new Data(TYPE_ITEM, "title" + i, "desp" + i, i);
+                    }
+                })
                 .collect(Collectors.toList());
     }
 }
--- app/src/main/java/com/example/recyclerview/MyAdapter.java
+++ app/src/main/java/com/example/recyclerview/MyAdapter.java
@@ -1,18 +1,17 @@
 package com.example.recyclerview;
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.View;
-import android.widget.TextView;
-
 import java.util.List;
 
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+public class MyAdapter extends RecyclerView.Adapter {
     List<Data> dataList;
 
     public MyAdapter(List<Data> dataList) {
@@ -21,18 +20,39 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = (View) LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.line_view, parent, false);
-        MyViewHolder vh = new MyViewHolder(v);
-        return vh;
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v;
+        switch (viewType) {
+            case Data.TYPE_HEADER:
+                v = (View) LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.head_bar, parent, false);
+                return new HeaderTextViewHolder(v);
+            case Data.TYPE_ITEM:
+                v = (View) LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.line_view, parent, false);
+                return new TextViewHolder(v);
+        }
+        return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.title.setText(dataList.get(position).title);
-        holder.number.setText(String.valueOf(dataList.get(position).number));
-        holder.description.setText(dataList.get(position).description);
+    public int getItemViewType(int position) {
+        return dataList.get(position).type;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        switch (dataList.get(position).type) {
+            case Data.TYPE_HEADER:
+                ((HeaderTextViewHolder) holder).headerText.setText(dataList.get(position).title);
+                break;
+            case Data.TYPE_ITEM:
+                ((TextViewHolder) holder).title.setText(dataList.get(position).title);
+                ((TextViewHolder) holder).number.setText(String.valueOf(dataList.get(position).number));
+                ((TextViewHolder) holder).description.setText(dataList.get(position).description);
+                break;
+        }
+
     }
 
     @Override
@@ -40,18 +60,29 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         return dataList.size();
     }
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
+    public static class TextViewHolder extends RecyclerView.ViewHolder {
         public View view;
         public TextView title;
         public TextView number;
         public TextView description;
 
-        public MyViewHolder(View v) {
+        public TextViewHolder(View v) {
             super(v);
             view = v;
             title = view.findViewById(R.id.title);
             number = view.findViewById(R.id.number);
             description = view.findViewById(R.id.description);
+        }
+    }
+
+    public static class HeaderTextViewHolder extends RecyclerView.ViewHolder {
+        public View view;
+        public TextView headerText;
+
+        public HeaderTextViewHolder(@NonNull View itemView) {
+            super(itemView);
+            view = itemView;
+            headerText = view.findViewById(R.id.header_text);
         }
     }
 
