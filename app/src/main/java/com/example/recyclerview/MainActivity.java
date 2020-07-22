6baffa99c172ef91bb910577ef3402b57edfa025
Long <longxu1@lenovo.com>
2020-07-22 17:49:23 +0800

    revert code

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,20 +1,13 @@
 package com.example.recyclerview;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @OnClick(R.id.recycler_view_btn)
-    void onClick() {
-        Intent intent = new Intent(this, ListActivity.class);
-        startActivity(intent);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
